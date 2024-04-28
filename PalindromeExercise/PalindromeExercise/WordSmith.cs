95e9a3108c34cae0519aab6dde4ff57c7d8b0782
Fikr Abebe <fikrabebe24@gmail.com>
2024-04-27 22:17:45 -0500

    Palindrome Exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsAPalindrome(string word)
+		{
+
+			var wordReversed = "";
+
+			for(int i = word.Length; i >= 0; i--)
+			{
+				wordReversed += word[i];
+			}
+
+
+
+			if(word == wordReversed)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+				
+		}
+
+		
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("madam", true)]
+        [InlineData("civic", true)]
+        [InlineData("Hello", false)]
+        public void Test1(string word, bool expected)
         {
+            //Arrange
+            var test = new WordSmith();
 
+            //Act
+            var actual = test.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(actual, expected);
         }
     }
 }
